029ed474809619a29964fb7cf10863b701de4a3a
Kaya <love4stephanie@protonmail.com>
2021-08-15 04:29:35 -0400

    practice

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -15,7 +15,8 @@ class LinkedList {
       }
 
       insertFirst(data){
-            this.head =  new Node(data, this.head);
+            // this.head =  new Node(data, this.head);
+            this.insertAt(data, 0);
       }
 
       size(){
@@ -30,10 +31,13 @@ class LinkedList {
       }
 
       getFirst(){
-            return this.head;
+            // return this.head;
+            return this.getAt(0);
       }
 
       getLast(){
+           return this.getAt(this.size() - 1);
+            /*
             if (!this.head){
                   return null;
             }
@@ -45,6 +49,8 @@ class LinkedList {
                         }
                         node = node.next;
                   }
+           
+           */
       }
 
       clear(){
@@ -59,7 +65,101 @@ class LinkedList {
       }
 
       removeLast(){
+            if(!this.head){
+                  return;
+            }
+
+            if(!this.head.next){
+                  this.head = null;
+                  return;
+            }
             
+            let previous = this.head;
+            let node = this.head.next;
+            while (node.next){
+                  previous = node;
+                  node = node.next;
+            }
+            previous.next = null;
+      }
+
+      insertLast(data){
+            const last = this.getLast();
+            if(last){
+                  //There are some existing nodes in out chain
+                 last.next = new Node(data);
+            }else {
+                  // The chain is empty
+                  this.head = new Node(data);
+            }
+      }
+
+      getAt(index){
+            let counter = 0;
+            let node = this.head;
+            while(node){
+                  if (counter === index){
+                        return node;
+                  }
+
+                  counter ++;
+                  node = node.next;
+
+            }
+
+            return null;
+      }
+
+      removeAt(index){
+            if(!this.head){
+                  return;
+            }
+
+            if(index === 0){
+                  this.head = this.head.next;
+                  return;
+            }
+
+            const previousNode  = this.getAt(index - 1);
+            if(!previousNode || !previousNode.next){
+                  return;
+            }
+            previousNode.next = previousNode.next.next;
+
+      }
+
+      insertAt(data, index){
+            if(!this.head){
+                  this.head = new Node(data);
+                  return;
+            }
+
+            if(index === 0 ){
+                  this.head = new Node(data, this.head);
+                  return;
+            }
+
+            const previous = this.getAt(index - 1) || this.getLast();
+            const node = new Node(data, previous.next);
+            previous.next = node;
+      }
+
+      forEach(fn){
+            let node = this.head;
+            let counter = 0;
+            while(node){
+                  fn(node, counter);
+                  node = node.next;
+                  counter++;
+            }
+      }
+
+      *[Symbol.iterator](){
+            let node = this.head;
+            while(node){
+                  yield node;
+                  node = node.next;
+            }
       }
 
 }
